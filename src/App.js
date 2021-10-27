35af917d631e821a90d3057745b762375718cfba
irispoon1022 <35031745+irispoon1022@users.noreply.github.com>
2021-10-27 06:00:12 +0000

    add comments to increase code readability

--- src/App.js
+++ src/App.js
@@ -2,31 +2,33 @@ import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import abi from "./utils/store.json";
+import quotes from "./utils/quotes.js";
 
 const App = () => {
+  
+  // the current connected metamask account
   const [currentAccount, setCurrentAccount] = useState("");
-  const [allWaves, setAllWaves] = useState([]);
+
+  // whether to display a random quote or not
   const [showQuote, setShowQuote] = useState(false);
+
+  // the random quote to be shown
   const [randomQuote, setRandomQuote] = useState('');
+
+  // input message in the input textbox
   const [message, setMessage] = useState('');
+
+  // whether the transaction is confirmed or not
   const [loading, setLoading] = useState(false);
+
+  // the past messages
+  const [allMessages, setAllMessages] = useState([]);
+
+  // contract details
   const contractAddress = "0x395758376e3C439330053b0B6b27a976204EbeE5";
   const contractABI = abi.abi;
 
-  const quotes = [
-    'Antifragility is beyond resilience or robustness. The resilient resists shocks and stays the same; the antifragile gets better.',
-    'My standpoint is that as long as it doesn’t harm other people’s free will, every explanation to one’s meaning of life can be celebrated as equal. No one has the authority to evaluate one’s interpretation to meaning of life.',
-    'Large numbers of people can collaborate by sharing common myths and beliefs.',
-    'Leadership creates the conditions for team effectiveness. It increases the possibility of team effectiveness. It does NOT ensures or causes team effectiveness.',
-    'The more members who are contributing to the real work of leadership (that is, helping to create, fine-tune, and exploit the benefits of the enabling conditions), the better.',
-    'It’s lonely at the top. Ninety-nine percent of people in the world are convinced they are incapable of achieving great things, so they aim for the mediocre. The level of competition is thus fiercest for “realistic” goals, paradoxically making them the most time-and energy-consuming. ',
-    'Bear with me. What is the opposite of happiness? Sadness? No. Just as love and hate are two sides of the same coin, so are happiness and sadness. Crying out of happiness is a perfect illustration of this. The opposite of love is indifference, and the opposite of happiness is—here’s the clincher—boredom.',
-    'There is no picture- or theory-independent concept of reality. Instead we will adopt a view that we will call model-dependent realism: the idea that a physical theory or world picture is a model (generally of a mathematical nature) and a set of rules that connect the elements of the model to observations. This provides a framework with which to interpret modern science.',
-    'If life brought events in front of me, I would treat them as if they came to take me beyond myself. If my personal self complained, I would use each opportunity to simply let him go and surrender to what life was presenting me. This was the birth of what I came to call “the surrender experiment,” and I was totally prepared to see where it would take me.',
-    'My formula for success was very simple: Do whatever is put in front of you with all your heart and soul without regard for personal results.'
-
-  ];
-
+  
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -46,7 +48,7 @@ const App = () => {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account);
-        getAllWaves();
+        getAllMessages();
       } else {
         console.log("No authorized account found")
       }
@@ -68,44 +70,45 @@ const App = () => {
       console.log("Connected", accounts[0]);
       /*then change the state of currentAccount to be the selected account */
       setCurrentAccount(accounts[0]);
-      getAllWaves();
+      getAllMessages();
     } catch (error) {
       console.log(error)
     }
   }
 
-  const getAllWaves = async () => {
+  const getAllMessages = async () => {
     try {
       const { ethereum } = window;
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const quoteJarContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        //Call the getAllWaves method from your Smart Contract
-        const waves = await wavePortalContract.getAllMessages();
-        console.log(waves);
+        //Call the getAllMessages method from your Smart Contract
+        const Messages = await quoteJarContract.getAllMessages();
+        console.log(Messages);
 
         // We only need address, timestamp, and message in our UI so let's pick those out
-        let wavesCleaned = [];
-        waves.forEach(wave => {
-          wavesCleaned.push({
-            address: wave.messanger,
-            timestamp: new Date(wave.timestamp * 1000),
-            message: wave.message
+        let MessagesCleaned = [];
+        Messages.forEach(message => {
+          MessagesCleaned.push({
+            address: message.messanger,
+            timestamp: new Date(message.timestamp * 1000),
+            message: message.message
           });
         });
 
-        wavesCleaned.sort((a,b) => a.timestamp < b.timestamp ? 1:-1);
+        //sort the array according to time (latest first)
+        MessagesCleaned.sort((a,b) => a.timestamp < b.timestamp ? 1:-1);
 
         // Store our data in React State
-        setAllWaves(wavesCleaned);
+        setAllMessages(MessagesCleaned);
 
-        // listen for events
-        wavePortalContract.on("NewMessage", (from, timestamp, message) => {
+        // listen for events (whenever new message comes,change the state)
+        quoteJarContract.on("NewMessage", (from, timestamp, message) => {
           console.log("NewMessage", from, timestamp, message);
           setLoading(false);
-          setAllWaves(prevState => [{
+          setAllMessages(prevState => [{
             address: from,
             timestamp: new Date(timestamp * 1000),
             message: message
@@ -132,22 +135,14 @@ const App = () => {
         setLoading(true);
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const StoreContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const quoteJarContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        let count = await StoreContract.getTotalMessages();
-        console.log("Retrieved total wave count...", count.toNumber());
+        //Execute the sendMessage function from your smart contract
+        const messageTxn = await quoteJarContract.sendMessage(message,{gasLimit:1000000});
+        console.log("Mining...", messageTxn.hash);
 
-        /*
-        * Execute the actual wave from your smart contract
-        */
-        const waveTxn = await StoreContract.sendMessage(message,{gasLimit:1000000});
-        console.log("Mining...", waveTxn.hash);
-
-        await waveTxn.wait();
-        console.log("Mined -- ", waveTxn.hash);
-
-        count = await StoreContract.getTotalMessages();
-        console.log("Retrieved total wave count...", count.toNumber());
+        await messageTxn.wait();
+        console.log("Mined -- ", messageTxn.hash);
 
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -157,9 +152,7 @@ const App = () => {
     }
   }
 
-  /**
-   * After the UI is rendered, check if wallet is connected
-   */
+  // After the UI is rendered, check if wallet is connected
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -172,6 +165,7 @@ const handleShowRandomQuote = () => {
   return (
     <div className="mainContainer">
       <div className="dataContainer">
+
         <div className="header">
           the Quote Jar
         </div>
@@ -180,11 +174,10 @@ const handleShowRandomQuote = () => {
           <p>I am Iris, a product manager passionate about cryptocurrencies. <br></br>
           Let me share with you some inspirational quotes I like.</p>
         </div>
-        <button className="waveButton" onClick={handleShowRandomQuote}>
+        <button className="messageButton" onClick={handleShowRandomQuote}>
           Show me a quote
         </button>
         <p className="blockQuote">{randomQuote}</p>
-
 
         <p className="bio" style={{"margin-bottom":"4px"}}> Share your favourate quote on the Rinkeby Ethereum network and win some Eths.</p>
         {!currentAccount && (
@@ -194,7 +187,8 @@ const handleShowRandomQuote = () => {
         <button className="secondaryButton" onClick={sendMessage}>
           Share
         </button>
-
+        
+        {/* loading state while the message is being confirmed */}
         {loading && <div style={{ backgroundColor: "OldLace", marginTop: "8px", padding: "16px",opacity:"50%" }} > 
         <div class="container">
           <div class="dash uno"></div>
@@ -204,11 +198,12 @@ const handleShowRandomQuote = () => {
         </div>
         Your message is being packaged into an Ethereum block (usually takes 10s). Take a sip of drink and come back for the magic ~ </div>}
 
-        {allWaves.map((wave, index) => {
+        {/* list of past messages */}
+        {allMessages.map((message, index) => {
           return (
             <div key={index} style={{ backgroundColor: "OldLace", marginTop: "8px", padding: "16px" }}>
-              <div className="comment">{wave.message}</div>
-              <div className="smallText">{wave.timestamp.toString()}</div>
+              <div className="comment">{message.message}</div>
+              <div className="smallText">{message.timestamp.toString()}</div>
               
             </div>)
         })}
---
+++ src/utils/quotes.js
@@ -0,0 +1,15 @@
+const quotes = [
+    'Antifragility is beyond resilience or robustness. The resilient resists shocks and stays the same; the antifragile gets better.',
+    'My standpoint is that as long as it doesn’t harm other people’s free will, every explanation to one’s meaning of life can be celebrated as equal. No one has the authority to evaluate one’s interpretation to meaning of life.',
+    'Large numbers of people can collaborate by sharing common myths and beliefs.',
+    'Leadership creates the conditions for team effectiveness. It increases the possibility of team effectiveness. It does NOT ensures or causes team effectiveness.',
+    'The more members who are contributing to the real work of leadership (that is, helping to create, fine-tune, and exploit the benefits of the enabling conditions), the better.',
+    'It’s lonely at the top. Ninety-nine percent of people in the world are convinced they are incapable of achieving great things, so they aim for the mediocre. The level of competition is thus fiercest for “realistic” goals, paradoxically making them the most time-and energy-consuming. ',
+    'Bear with me. What is the opposite of happiness? Sadness? No. Just as love and hate are two sides of the same coin, so are happiness and sadness. Crying out of happiness is a perfect illustration of this. The opposite of love is indifference, and the opposite of happiness is—here’s the clincher—boredom.',
+    'There is no picture- or theory-independent concept of reality. Instead we will adopt a view that we will call model-dependent realism: the idea that a physical theory or world picture is a model (generally of a mathematical nature) and a set of rules that connect the elements of the model to observations. This provides a framework with which to interpret modern science.',
+    'If life brought events in front of me, I would treat them as if they came to take me beyond myself. If my personal self complained, I would use each opportunity to simply let him go and surrender to what life was presenting me. This was the birth of what I came to call “the surrender experiment,” and I was totally prepared to see where it would take me.',
+    'My formula for success was very simple: Do whatever is put in front of you with all your heart and soul without regard for personal results.'
+
+  ];
+
+export default quotes
